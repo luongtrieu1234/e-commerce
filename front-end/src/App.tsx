1b0c729bb2b91cb2fa688f60ef67a6ba01d400e9
luongtrieu1234 <luongtrieu1234a@gmail.com>
2024-12-03 21:12:13 +0700

    init: create code base

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,11 @@
-
-import './App.css'
+import "./App.css";
+import Home from "./features/Home";
 
 function App() {
   type User = {
     name: string;
     age: number;
   };
-
 
   const user: User = {
     name: "Alice",
@@ -15,11 +14,10 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello {user.name}
-    </h1>
+      <h1 className="text-3xl font-bold text-red-500">Hello {user.name}</h1>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ front-end/src/features/Home/index.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <>
+      <h1 className="text-7xl font-bold underline text-green-600">
+        Home Page
+      </h1>
+      </>
+    )
+  }
+  
+  export default Home
