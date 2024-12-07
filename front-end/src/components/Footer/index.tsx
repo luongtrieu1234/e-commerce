faf1c34a768082eabd310fdfc40840b385818870
luongtrieu1234 <luongtrieu1234a@gmail.com>
2024-12-07 16:15:14 +0700

    feat(components): build footer component

--- front-end/src/components/Footer/index.tsx
+++ front-end/src/components/Footer/index.tsx
@@ -1,8 +1,77 @@
+import {
+  FaFacebook,
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaComments,
+} from "react-icons/fa";
+
 function Footer() {
   return (
-    <>
-      <h1 className="text-7xl font-bold text-green-600">Footer</h1>
-    </>
+    <footer className="bg-gray-800 text-white p-6">
+      <div className="container mx-auto flex flex-col md:flex-row md:justify-start md:gap-36">
+        <div className="mb-4 md:mb-0 text-left max-w-xl">
+          <h1 className="text-2xl font-bold text-blue-400 mb-4">
+            Thông Tin Công Ty
+          </h1>
+          <h1 className="text-xl font-bold mb-2 text-orange-500">
+            CÔNG TY TNHH CHÍNH THỰC
+          </h1>
+          <p className="mb-2 flex items-start flex-wrap">
+            <FaMapMarkerAlt className="mr-2 mt-1 flex-shrink-0" color="red" />
+            <strong className="whitespace-nowrap flex-shrink-0">Map:</strong>
+            <span className="flex-grow">
+              &nbsp;Ô7 Lô Oxh, Đường D2, Khu nhà ở An Phú 1, Khu phố 1B, Phường
+              An Phú, Thành phố Thuận An, Tỉnh Bình Dương, Việt Nam
+            </span>
+          </p>
+        </div>
+        <div className="mb-4 md:mb-0 text-left">
+          <h1 className="text-2xl font-bold text-blue-400 mb-4">
+            Kết Nối Với Chúng Tôi
+          </h1>
+          <p className="mb-2 flex items-center">
+            <FaComments className="mr-2" color="green" /> <strong>Zalo:</strong>
+            &nbsp;0916412896
+          </p>
+          <p className="mb-2 flex items-center">
+            <FaPhone className="mr-2" color="blue" />{" "}
+            <strong>Điện Thoại:</strong>
+            &nbsp;0916412896
+          </p>
+          <p className="mb-2 flex items-center">
+            <FaEnvelope className="mr-2" color="yellow" />{" "}
+            <strong>Mail:</strong>&nbsp;
+            <a
+              href="mailto:maycatchinhthuc@gmail.com"
+              className="text-blue-400 ml-1"
+            >
+              maycatchinhthuc@gmail.com
+            </a>
+          </p>
+          <p className="mb-2 flex items-center">
+            <FaFacebook className="mr-2" color="blue" />{" "}
+            <strong>Facebook:</strong>&nbsp;
+            <a
+              href="https://www.facebook.com/profile.php?id=61558399526686"
+              className="text-blue-400 ml-1 truncate"
+              style={{
+                maxWidth: "200px",
+                display: "inline-block",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
+            >
+              https://www.facebook.com/profile.php?id=61558399526686
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="bg-blue-700 text-center text-xs py-4 mt-4">
+        <p>© Bản quyền thuộc về Công ty TNHH Chính Thực</p>
+      </div>
+    </footer>
   );
 }
 
--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -10,7 +10,8 @@ function Header() {
   console.log(isMenuOpen);
 
   return (
-    <header className="bg-gray-500 text-white">
+    // PC
+    <header className="bg-gray-800 text-white">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
@@ -27,7 +28,7 @@ function Header() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
             onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
@@ -65,10 +66,12 @@ function Header() {
           </a>
         </div>
       </nav>
+
+      {/* Mobile */}
       {isMenuOpen && (
         <div className="lg:hidden" role="dialog" aria-modal="true">
           <div className="fixed inset-0 z-10"></div>
-          <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-500 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 text-white">
+          <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-800 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 text-white">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <img
